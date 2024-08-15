d6b3a9f864a7bd61e2d8f236ad2cab9958f5446a
SageLovedayy <116376154+SageLovedayy@users.noreply.github.com>
2024-08-15 23:07:47 +0100

    Implement end-point to register a user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+from sqlalchemy.orm.exc import NoResultFound
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"])
@@ -12,6 +15,30 @@ def home():
         -JSON payload
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def new_user() -> str:
+    """POST /users
+    Registers new user with email and password, checks
+    if user is already registered
+    Return:
+        -JSON payload
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        new_user = AUTH.register_user(email, password)
+        if new_user is not None:
+            return jsonify({
+                "email": new_user.email,
+                "message": "user created"
+            })
+    except ValueError:
+        return jsonify({
+            "message": "email already registered"
+        }), 400
 
 
 if __name__ == "__main__":
