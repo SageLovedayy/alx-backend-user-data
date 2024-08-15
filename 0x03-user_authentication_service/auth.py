143fd21e46a10b3434b7b3b1ee956e273e0d8dec
SageLovedayy <116376154+SageLovedayy@users.noreply.github.com>
2024-08-15 22:12:48 +0100

    Add hash password method

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+from bcrypt import hashpw, gensalt
+
+def _hash_password(password: str) -> str:
+    """Returns salted hashed password as bytestring"""
+    return hashpw(password.encode("utf-8"), gensalt())
