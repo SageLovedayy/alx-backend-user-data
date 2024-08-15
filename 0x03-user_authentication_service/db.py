a3ab7cc7cc544c39123a7cc627db0a0acb4a574d
SageLovedayy <116376154+SageLovedayy@users.noreply.github.com>
2024-08-15 15:56:00 +0100

    Implement find by User

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,6 +6,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -37,3 +39,15 @@ class DB:
         self._session.add(newUser)
         self._session.commit()
         return newUser
+
+    def find_user_by(self, **kwargs) -> User:
+        """Returns first row of users table filtered by
+        input arguments"""
+        try:
+            searchResult = self._session.query(User).filter_by(**kwargs).first()
+        except TypeError:
+            raise InvalidRequestError
+        if searchResult is None:
+            raise NoResultFound
+        return searchResult
+
