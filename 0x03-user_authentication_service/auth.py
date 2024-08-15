e0ef8230198941309b86f46548703fbe7400098c
SageLovedayy <116376154+SageLovedayy@users.noreply.github.com>
2024-08-15 23:31:47 +0100

    Implement method to check valid login

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from bcrypt import hashpw, gensalt
+from bcrypt import hashpw, gensalt, checkpw
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
@@ -20,7 +20,7 @@ class Auth:
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
-        """Registes new user if email does not exist in db"""
+        """Registers new user if email does not exist in db"""
         try:
             self._db.find_user_by(email=email)
             raise ValueError(f"User {email} already exists")
@@ -28,3 +28,15 @@ class Auth:
             hashed_password = _hash_password(password)
             new_user = self._db.add_user(email, hashed_password)
             return new_user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Implements credential validation"""
+        try:
+            user_found = self._db.find_user_by(email=email)
+            return checkpw(
+                    password.encode("utf-8"),
+                    user_found.hashed_password
+                    )
+
+        except NoResultFound:
+            return False
