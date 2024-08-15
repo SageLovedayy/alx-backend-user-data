62fc5063af4439d5acf0cc422e03930c210a833d
SageLovedayy <116376154+SageLovedayy@users.noreply.github.com>
2024-08-16 00:30:39 +0100

    Implement Login function

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 from sqlalchemy.orm.exc import NoResultFound
 
@@ -40,6 +40,27 @@ def new_user() -> str:
             "message": "email already registered"
         }), 400
 
+
+@app.route("/sessions", methods=["POST"])
+def login() -> str:
+    """POST /sessions
+    Creates new user session and stores as cookie
+    Return:
+        -JSON payload
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    session_id = AUTH.create_session(email)
+    response = jsonify({
+        "email": email,
+        "message": "logged in"
+    })
+    response.set_cookie("session_id", session_id)
+    return response
+    
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
