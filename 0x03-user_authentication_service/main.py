012c9eae60fdcead5fc2e0cf1bcbd89926a3f680
SageLovedayy <116376154+SageLovedayy@users.noreply.github.com>
2024-08-16 01:39:48 +0100

    Implement update password01

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,18 +1,62 @@
 #!/usr/bin/env python3
-"""
-Main file
-"""
-from auth import Auth
+""" End-to-end integration test."""
 
-email = 'bob@bob.com'
-password = 'MyPwdOfBob'
-auth = Auth()
+import requests
 
-auth.register_user(email, password)
 
-print(auth.valid_login(email, password))
+def register_user(email: str, password: str) -> None:
+    """ Tests register_user."""
+    assert True
 
-print(auth.valid_login(email, "WrongPwd"))
 
-print(auth.valid_login("unknown@email", password))
+def log_in_wrong_password(email: str, password: str) -> None:
+    """ Test logging in with wrong password."""
+    assert True
 
+
+def log_in(email: str, password: str) -> str:
+    """ Test log-in."""
+    assert True
+
+
+def profile_unlogged() -> None:
+    """ Test profile unlogged."""
+    assert True
+
+
+def profile_logged(session_id: str) -> None:
+    """ Test profile logged."""
+    assert True
+
+
+def log_out(session_id: str) -> None:
+    """ Test logout."""
+    assert True
+
+
+def reset_password_token(email: str) -> str:
+    """ Test reset password."""
+    assert True
+
+
+def update_password(email: str, reset_token: str, new_password: str) -> None:
+    """ Test Update password."""
+    assert True
+
+
+EMAIL = "guillaume@holberton.io"
+PASSWD = "b4l0u"
+NEW_PASSWD = "t4rt1fl3tt3"
+
+
+if __name__ == "__main__":
+
+    register_user(EMAIL, PASSWD)
+    log_in_wrong_password(EMAIL, NEW_PASSWD)
+    profile_unlogged()
+    session_id = log_in(EMAIL, PASSWD)
+    profile_logged(session_id)
+    log_out(session_id)
+    reset_token = reset_password_token(EMAIL)
+    update_password(EMAIL, reset_token, NEW_PASSWD)
+    log_in(EMAIL, NEW_PASSWD)
